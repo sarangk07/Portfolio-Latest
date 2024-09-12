263102bcbdc7bf2c58f9442ab50052f0336078f7
Sarang <sarang00005@gmail.com>
2024-09-12 08:56:51 +0530

    new-project-added

--- app/page.jsx
+++ app/page.jsx
@@ -260,7 +260,7 @@ export default function Home() {
             <div className=' flex md:mt-5 mt-0 ml-4 justify-center relative bottom-6'>
                 
                 <svg
-                 className={`w-10 h-10 ${choiceTheme == 'white'|| 'dark' ? 'text-zinc-400 hover:text-black transform hover:duration-200' : ''} `} viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 className={`w-10 h-10 ${choiceTheme == 'white' ? 'hover:text-zinc-400 text-black transform hover:duration-200' : 'text-white'} `} viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path 
                     fillRule="evenodd" 
                     clipRule="evenodd" 
@@ -270,7 +270,7 @@ export default function Home() {
                 </svg>
 
                 <a href="https://www.linkedin.com/in/sarang-k-2b7844244/">
-                <svg className={`w-10 h-10 ${choiceTheme == 'white'|| 'dark' ?  'text-zinc-400 hover:text-blue-600 transform hover:duration-200' : ''} `} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 107.3 107.3">
+                <svg className={`w-10 h-10 ${choiceTheme == 'white' ?  'hover:text-zinc-400 text-blue-600 transform hover:duration-200' : 'text-white'} `} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 107.3 107.3">
                   <g id="Layer_2" data-name="Layer 2">
                     <g data-name="图层 1">
                       <path 
@@ -291,7 +291,7 @@ export default function Home() {
 
 
                 <a href="https://github.com/sarangk07">
-                <svg className={`w-10 h-10 ${choiceTheme == 'white'|| 'dark' ? 'text-zinc-400 hover:text-white transform hover:duration-200' : ''} `} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
+                <svg className={`w-10 h-10 ${choiceTheme == 'white' ? 'hover:text-zinc-400 text-zinc-900 transform hover:duration-100' : 'text-white'} `} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128">
                   <g fill="currentColor">
                     <path fillRule="evenodd" d="M64 5.103c-33.347 0-60.388 27.035-60.388 60.388 0 26.682 17.303 49.317 41.297 57.303 3.017.56 4.125-1.31 4.125-2.905 0-1.44-.056-6.197-.082-11.243-16.8 3.653-20.345-7.125-20.345-7.125-2.747-6.98-6.705-8.836-6.705-8.836-5.48-3.748.413-3.67.413-3.67 6.063.425 9.257 6.223 9.257 6.223 5.386 9.23 14.127 6.562 17.573 5.02.542-3.903 2.107-6.568 3.834-8.076-13.413-1.525-27.514-6.704-27.514-29.843 0-6.593 2.36-11.98 6.223-16.21-.628-1.52-2.695-7.662.584-15.98 0 0 5.07-1.623 16.61 6.19C53.7 35 58.867 34.327 64 34.304c5.13.023 10.3.694 15.127 2.033 11.526-7.813 16.59-6.19 16.59-6.19 3.287 8.317 1.22 14.46.593 15.98 3.872 4.23 6.215 9.617 6.215 16.21 0 23.194-14.127 28.3-27.574 29.796 2.167 1.874 4.097 5.55 4.097 11.183 0 8.08-.07 14.583-.07 16.572 0 1.607 1.088 3.49 4.148 2.897 23.98-7.994 41.263-30.622 41.263-57.294C124.388 32.14 97.35 5.104 64 5.104z" clipRule="evenodd"></path>
                     <path d="M26.484 91.806c-.133.3-.605.39-1.035.185-.44-.196-.685-.605-.543-.906.13-.31.603-.395 1.04-.188.44.197.69.61.537.91zm-.743-.55M28.93 94.535c-.287.267-.85.143-1.232-.28-.396-.42-.47-.983-.177-1.254.298-.266.844-.14 1.24.28.394.426.472.984.17 1.255zm-.575-.618M31.312 98.012c-.37.258-.976.017-1.35-.52-.37-.538-.37-1.183.01-1.44.373-.258.97-.025 1.35.507.368.545.368 1.19-.01 1.452zm0 0M34.573 101.373c-.33.365-1.036.267-1.552-.23-.527-.487-.674-1.18-.343-1.544.336-.366 1.045-.264 1.564.23.527.486.686 1.18.333 1.543zm0 0M39.073 103.324c-.147.473-.825.688-1.51.486-.683-.207-1.13-.76-.99-1.238.14-.477.823-.7 1.512-.485.683.206 1.13.756.988 1.237zm0 0M44.016 103.685c.017.498-.563.91-1.28.92-.723.017-1.308-.387-1.315-.877 0-.503.568-.91 1.29-.924.717-.013 1.306.387 1.306.88zm0 0M48.614 102.903c.086.485-.413.984-1.126 1.117-.7.13-1.35-.172-1.44-.653-.086-.498.422-.997 1.122-1.126.714-.123 1.354.17 1.444.663zm0 0"></path>
@@ -491,7 +491,7 @@ export default function Home() {
                     loading='lazy'
                   />
 
-                  <p className='hidden md:flex text-md hover:text-black'>Developed a website for car accesseries with Nextjs,Tailwindcss,Redux,GSAP</p>
+                  <p className='hidden md:flex md:flex-col text-md hover:text-black'>Developed a website for car accesseries with <span className='text-[17px] font-bold'>Nextjs,Tailwindcss,Redux,GSAP,Python-Django,DRF,SQLlite</span></p>
 
                 </div>
                 <div className={`
@@ -527,7 +527,7 @@ export default function Home() {
                     />
                    
                    </a>
-                  <p className='hidden md:flex text-md hover:text-black'>Developed a Rent a Car website using python Django,JWT,SMTP,AWS,Razorpay</p>
+                  <p className='hidden md:flex text-md hover:text-black'>Developed a Rent a Car website using python Django,PSQL,JWT,SMTP,AWS,Razorpay</p>
 
                 </div>
                 <div className={`
@@ -571,6 +571,45 @@ export default function Home() {
                   hidden md:flex h-full w-2 absolute top-1 -left-64 `} />
 
               </div>
+
+
+              <div className={`relative right-11  md:right-32 mt-5 md:mt-0 project-item md:ml-5 ml-10 mb-3 w-fit  ${choiceTheme == 'white' ? 'bg-zinc-100' : ''} bg-pixel-pattern bg-pixel p-1 cursor-pointer`}>
+                  <div className={`
+                    ${choiceTheme == 'red' ?  ' text-zinc-100' : choiceTheme == 'blue' ? 'text-zinc-100' : choiceTheme == 'white' ? 'bg-zinc-200 text-zinc-400' : 'bg-stone-950 text-gray-200'}  w-72 h-44 md:w-[580px] md:h-96 pl-4 pr-4 md:bg-transparent
+                    
+                    `}>
+
+                      <div className='flex justify-between'>
+                      <p>
+                        QuZ <span className='text-xs'> <a href="https://github.com/sarangk07/quz" target="_blank">- git</a></span>
+                      </p>
+                      <p className='flex text-xs md:text-sm cursor-default'>
+                        Mini-Project
+                      </p>
+                    </div>
+                  <a href="https://quz-game.vercel.app/" target="_blank">
+                  {/* <img  className={`${choiceTheme == 'red' ?  'shadow-custom-red ' : choiceTheme == 'blue' ? 'shadow-custom-blue' :'shadow-md'} md:w-fit md:h-72 h-44 w-64`} src="https://i.pinimg.com/564x/d8/fe/c7/d8fec7801132a9a4f9c530b98396e295.jpg" alt="" loading='lazy' data-src="./social-meadia.PNG" /> */}
+                  
+                  <Image
+                    className={`${choiceTheme == 'red' ? 'shadow-custom-red' : choiceTheme == 'blue' ? 'shadow-custom-blue' : 'shadow-md'} opacity-[0.9] md:w-fit md:h-72 h-44 w-64`}
+                    src="/quz.PNG"
+                    alt="qz"
+                    width={500}
+                    height={300}
+                    loading='lazy'
+                  />
+                  
+                  </a>
+                  <p className='hidden md:flex text-md hover:text-black'>Developed a Quiz-game using NEXTJS ,Open-Trivia API ,Tailwindcss</p>
+                </div>
+                <div className={`
+                  ${choiceTheme == 'red' ? 'bg-red-600 animate-glow' : choiceTheme == 'blue' ? 'bg-blue-600 animate-glowBlue' : choiceTheme == 'white' ? 'bg-zinc-400' : 'bg-zinc-800'}
+                  hidden md:flex h-full w-2 absolute top-1 -right-64 `} />
+
+              </div>
+
+
+
             </div>
           </div>
 
