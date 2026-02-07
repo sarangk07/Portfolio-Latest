283f6eb07838d1383f5bc1ede8ffbf2f35db6170
sarangk07 <sarang00005@gmail.com>
2026-02-07 21:24:01 +0530

    new fav icon

--- app/layout.js
+++ app/layout.js
@@ -3,8 +3,10 @@ import "./globals.css";
 export const metadata = {
   title: "Portfolio",
   description: "Developed With NextJS",
+  // Next.js will automatically use app/icon.svg if present, 
+  // but we can clean up the explicit reference to the old favicon.ico
   icons: {
-    icon: '/favicon.ico',
+    icon: '/icon.svg', // Explicitly pointing to our new SVG just in case, though Next.js auto-discovers in app/
     apple: '/apple-touch-icon.png',
   },
   manifest: '/site.webmanifest',
